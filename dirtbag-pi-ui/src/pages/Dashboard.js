f5487fba1c4390bb632684c8de6a04489972b322
Matt Hambro <matthew.hambro@gmail.com>
2020-12-29 22:00:29 -0800

    Use signed URL to fetch graph

--- dirtbag-pi-ui/src/pages/Dashboard.js
+++ dirtbag-pi-ui/src/pages/Dashboard.js
@@ -1,30 +1,8 @@
 import React, { Component } from 'react';
 import Unauthenticated from './Unauthenticated';
-import { Storage } from 'aws-amplify'
 
 
 class Dashboard extends Component {
-
-   async componentDidMount() {
-
-      const data = await Storage.list()
-      console.log(data)
-
-    // try {
-    //   const data = await Storage.list()
-    //   console.log(data)
-
-    // } catch(error) {
-    //   let err = null;
-    //   !error.message ? err = {"message": error} : err = error
-    //   this.setState({
-    //     errors: {
-    //       ...this.state.errors,
-    //       cognito: err
-    //     }
-    //   })
-    // }
-  };
 
 
   render() {
--- dirtbag-pi-ui/src/pages/GraphBrowser.js
+++ dirtbag-pi-ui/src/pages/GraphBrowser.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import Unauthenticated from './Unauthenticated';
 import Iframe from 'react-iframe'
+import { Storage } from 'aws-amplify'
 
 class GraphBrowser extends Component {
+  state = {
+    indexURL: ""
+  }
+
+  async componentDidMount() {
+    
+    Storage.get('index.html')
+    .then(result => {
+      console.log(result)
+      this.setState({indexURL: result
+      })})
+    .catch(err => console.log(err))
+    console.log('result: ' + this.state.indexURL)
+  }
 
   render() {
+    
     return (
       <div >
         {
           this.props.auth.isAuthenticated ? (
-            <Iframe url="http://dirtbag-example-bucket-20200815222709639700000001.s3-website.us-east-2.amazonaws.com/"
+            <Iframe url={this.state.indexURL}
             width="90%"
             height="800px"
             id="myId"
